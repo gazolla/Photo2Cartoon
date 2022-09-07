3754d4c66404716e790472b440a2d5c6fa3a31de
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-09-06 21:06:54 -0300

    Initial Commit

---
+++ Photo2Cartoon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo2Cartoon
+//
+//  Created by sebastiao Gazolla Costa Junior on 06/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo2Cartoon/Photo2CartoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo2CartoonApp.swift
+//  Photo2Cartoon
+//
+//  Created by sebastiao Gazolla Costa Junior on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Photo2CartoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
